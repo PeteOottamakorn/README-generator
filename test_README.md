c2a424850050849e4e3921ac837cb09505084006
ItsToastCrumb <160540794+ItsToastCrumb@users.noreply.github.com>
2024-03-24 11:27:24 -0600

    fleshing out markdown creation, fixed license choices in order to create badge correctly

---
+++ test_README.md
@@ -0,0 +1,50 @@
+
+  # test
+  ![License](https://img.shields.io/badge/License-Apache_2.0-skyblue.svg)
+
+  ## Description
+
+  test
+
+  ## Table of Contents
+
+  [Installation](#installation)
+
+  [Usage](#usage)
+
+  [Contributing](#contributing)
+
+  [Credits](#credits)
+
+  [License](#license)
+
+  ## Installation
+  
+  test
+
+  ## Usage
+
+  test
+
+  ## Contributing
+
+  test
+
+  ## Tests
+
+  test
+
+  ## Credits
+
+
+  
+    ## License
+    
+    Please see the license file in the GitHub repository for more information
+
+  ## Questions
+
+  If there are any questions, you may visit my GitHub page at
+  https://github.com/test
+
+  Or feel free to contact me at test.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,84 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  //Can use a !== 'None' to determine outcome
+  if (license !== "None") {
+    //Create the badge
+    return `![License](https://img.shields.io/badge/License-${license}-skyblue.svg)`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  //Link to add to Table of Contents if there is a license section
+  if (license !== "None") {
+    //Link
+    return `[License](#license)`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  //Create the entire license section if there is a license present
+  if (license !== "None") {
+    return `
+    ## License
+    
+    Please see the license file in the GitHub repository for more information`;
+  }
+  return "";
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}\n\n## Description\n\n${data.description}\n\n## Table of Contents\n\nInsert Table of Contents here\n\n## Installation\n\n${data.install}\n\n## Usage\n\n${data.usage}\n\n## Contributing\n\n${data.contribute}\n\n## Tests\n\n${data.test}\n\n## License\n\nLicense function goes here\n\n## Questions\n\nIf there are any questions, you may visit my GitHub page at\nhttps://github.com/${data.username}\nOr feel free to contact me at ${data.email}.`;
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
+  return `
+  # ${data.title}
+  ${licenseBadge}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  [Installation](#installation)\n
+  [Usage](#usage)\n
+  [Contributing](#contributing)\n
+  [Credits](#credits)\n
+  ${licenseLink}
+
+  ## Installation
+  
+  ${data.install}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contributing
+
+  ${data.contribute}
+
+  ## Tests
+
+  ${data.test}
+
+  ## Credits\n
+
+  ${licenseSection}
+
+  ## Questions
+
+  If there are any questions, you may visit my GitHub page at
+  https://github.com/${data.username}\n
+  Or feel free to contact me at ${data.email}.`;
 }
 
 module.exports = generateMarkdown;
